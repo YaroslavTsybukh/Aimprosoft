7b191ec403a5663bc21b3653123f8ae9b03ac1af
Yaroslav Tsybukh <yaroslavcybuh26092001@gmail.com>
2020-12-08 19:35:26 +0200

    desktop version full

--- First/js/main.js
+++ First/js/main.js
@@ -25,3 +25,21 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
+
+const menuToggle = document.querySelector('#menu-togle');
+const hamburger = document.querySelector('#hamburger');
+
+menuToggle.addEventListener('click',function(){
+    if(menuToggle){
+        menuToggle.display = "none"
+        menuToggle.classList.toggle('menu-icon-active');
+        hamburger.classList.toggle('hamburger--active')
+    }else{
+        menuToggle.display = "block"
+        menuToggle.classList.toggle('menu-icon-active');
+        hamburger.classList.toggle('hamburger--active');
+    };//Метод toggle объекта classList чередует заданный CSS класс элемента: добавляет класс, если его нет и удаляет, если есть. 
+})
