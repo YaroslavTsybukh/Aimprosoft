409d5a055070ec2982f2102b8f368d59ae0ad4a5
Yaroslav Tsybukh <yaroslavcybuh26092001@gmail.com>
2020-12-10 03:07:39 +0200

    mobile 768

--- First/js/main.js
+++ First/js/main.js
@@ -31,15 +31,33 @@ $('.owl-carousel').owlCarousel({
 
 const menuToggle = document.querySelector('#menu-togle');
 const hamburger = document.querySelector('#hamburger');
-
+const hamburgerMobile = document.querySelector('#mobilehamburger')
+const menuToggleMobile = document.querySelector('#mobilemenu-togle')
 menuToggle.addEventListener('click',function(){
     if(menuToggle){
         menuToggle.display = "none"
         menuToggle.classList.toggle('menu-icon-active');
         hamburger.classList.toggle('hamburger--active')
+        menuToggleMobile.display = "none"
+        menuToggleMobile.classList.toggle('menu-icon-active');
+        hamburgerMobile.classList.toggle('mobilehamburger--active')
     }else{
         menuToggle.display = "block"
         menuToggle.classList.toggle('menu-icon-active');
         hamburger.classList.toggle('hamburger--active');
+        menuToggleMobile.display = "block"
+        menuToggleMobile.classList.toggle('menu-icon-active');
+        hamburgerMobile.classList.toggle('mobilehamburger--active')
+    };//Метод toggle объекта classList чередует заданный CSS класс элемента: добавляет класс, если его нет и удаляет, если есть. 
+})
+menuToggleMobile.addEventListener('click',function(){
+    if(menuToggleMobile){
+        menuToggleMobile.display = "none"
+        menuToggleMobile.classList.toggle('menu-icon-active');
+        hamburgerMobile.classList.toggle('hamburger--active')
+    }else{
+        menuToggleMobile.display = "block"
+        menuToggleMobile.classList.toggle('menu-icon-active');
+        hamburgerMobile.classList.toggle('hamburger--active')
     };//Метод toggle объекта classList чередует заданный CSS класс элемента: добавляет класс, если его нет и удаляет, если есть. 
 })
